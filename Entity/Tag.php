b2f2328af7c63447c5ea4b0640d54cd6e0c9c61c
mgiagnoni <gimassimo@gmail.com>
2012-07-11 11:11:02 +0200

    Created tag model

---
+++ Entity/Tag.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace MGI\ClassifiedsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MGI\ClassifiedsBundle\Entity\Tag
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="MGI\ClassifiedsBundle\Entity\TagRepository")
+ */
+class Tag
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var text $description
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param text $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Get description
+     *
+     * @return text
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+}
